05a264a475891b5b3362aa033cb9707060d234ec
gaspcr <riugarte@uc.cl>
2022-12-02 20:43:27 -0300

    dsa

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
 <?php include("template/cabecera.php"); ?>
             <body>
         
-            <form action="login.php" method="post">
-        
+            <form action="sesion.php" method="post">
                 <h2>LOGIN</h2>
-        
                 <?php if (isset($_GET['error'])) { ?>
-                
                     <p class="error"><?php echo $_GET['error']; ?></p>
-                
                 <?php } ?>
-                
                 <label>User Name</label>
-                
-                <input type="text" name="uname" placeholder="User Name"><br>
-                
+                <input type="text" name="user" placeholder="User Name"><br>
                 <label>Password</label>
-                
                 <input type="password" name="password" placeholder="Password"><br> 
-                
                 <button type="submit">Login</button>
-                
             </form>
                 
             </body>
+
             <?php
             require("config/conexion.php");
             $result = $db -> prepare("SELECT * FROM users;");
---
+++ sesion.php
@@ -0,0 +1,16 @@
+<?php
+require("config/conexion.php");
+session_start();
+$usuario = $_POST['user'];
+$contrasena = $_POST['password'];
+
+$result = $db -> prepare("SELECT * FROM users WHERE nombre = '$usuario' AND password = '$contrasena';");
+$result -> execute();
+$dataCollected = $result -> fetchAll();
+if (sizeof($dataCollected) == 0) {
+    header("Location: index.php?error=Usuario o contraseña incorrectos");
+} else {
+    $_SESSION['user'] = $usuario;
+    header("Location: home.php");
+}
+?>
