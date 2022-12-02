af4956929d5bba48c065fc8216ecb68a591451d1
gaspcr <riugarte@uc.cl>
2022-12-02 00:03:34 -0300

    a

--- importar_usuarios.php
+++ importar_usuarios.php
@@ -13,6 +13,21 @@ if ($result) {
 } else {
     echo "Hubo un error al importar los usuarios, por favor intenta vaciar la tabla USERS";
 }
+
+$result1 = $db1 -> prepare("SELECT * FROM users;");
+$result1 -> execute();
+$dataCollected1 = $result1 -> fetchAll();
+?>
+
+<?php
+foreach ($dataCollected1 as $d1) {
+    echo "<tr>";
+    echo "<td>$d1[0]</td>";
+    echo "<td>$d1[1]</td>";
+    echo "<td>$d1[2]</td>";
+    echo "<td>$d1[3]</td>";
+    echo "</tr>";
+}
 ?>
 
 <a href="../index.php" class="btn btn-primary">Volver</a>
