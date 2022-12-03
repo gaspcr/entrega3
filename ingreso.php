13478d92d8abd384177958d87ea7a13f9aded2f6
gaspcr <riugarte@uc.cl>
2022-12-02 22:12:22 -0300

    dsa

--- ingreso.php
+++ ingreso.php
@@ -3,7 +3,7 @@ session_start();
 $usuario = $_SESSION['user'];
 if (isset($usuario)) {
     echo "<h1>Bienvenido $usuario</h1>";
-    echo "<a href='queries/logout.php'>Cerrar sesión</a>";
+    echo "<a href='logout.php'>Cerrar sesión</a>";
 } else {
     header("Location: index.php");
 }
