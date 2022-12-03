51bb0bb20d17bc11a4bd44b36c79262cc7db0c32
gaspcr <riugarte@uc.cl>
2022-12-02 22:06:54 -0300

    dsads

--- ingreso.php
+++ ingreso.php
@@ -3,3 +3,7 @@ session_start();
 $usuario = $_SESSION['user'];
 
 echo "<h1>BIENVENIDO $usuario</h1>";
+
+echo "<form align='center' action='queries/logout.php' method='post'>
+    <input type='submit' value='LOGOUT'>
+    </form>";
